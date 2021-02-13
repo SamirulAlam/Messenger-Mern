b951a86bc25a8b0028fc18468b7aa807a6fcdac7
Samirul Alam <samirulalam372@gmail.com>
2021-02-14 04:00:12 +0600

    frontend completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, {useState,useEffect} from 'react';
+import { Button,FormControl,InputLabel,Input } from '@material-ui/core';
 import './App.css';
+import Message from './Components/Message';
+import FlipMove from "react-flip-move";
+import SendIcon from '@material-ui/icons/Send';
+import { IconButton } from '@material-ui/core';
 
 function App() {
+  const [input,setInput]=useState("");
+  const [messages,setMessages]=useState([]);
+  const [username,setUsername]=useState("");
+
+  useEffect(()=>{
+    
+  },[])
+
+  useEffect(() => {
+    setUsername(prompt("Enter your name"));
+  }, []);
+
+  const sendMessage=(event)=>{
+    setInput("");
+  }
   return (
-    <div className="app">
-      
+    <div className="App">
+      <img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" />
+      <h1>Messenger Clone</h1>
+  <h2>Welcome {username}</h2>
+      <form className="app__form">
+      <FormControl className="app__formControl">
+        <InputLabel>Write message</InputLabel>
+        <Input className="app__input" value={input} onChange={event => setInput(event.target.value)} />
+        <IconButton className="app__iconButton" disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>
+          <SendIcon />
+        </IconButton>
+      </FormControl>
+      </form>
+      <FlipMove>
+      {
+        messages.map(({id,message})=>(
+          <Message key={id} username={username} message={message} />     
+        ))
+      }
+      </FlipMove>
     </div>
   );
 }
