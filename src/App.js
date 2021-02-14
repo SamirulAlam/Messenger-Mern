304e210fbd3d17b6a1a5a1bfcb87927ecedb118d
Samirul Alam <samirulalam372@gmail.com>
2021-02-15 01:29:01 +0600

    added with the backend

--- src/App.js
+++ src/App.js
@@ -5,21 +5,51 @@ import Message from './Components/Message';
 import FlipMove from "react-flip-move";
 import SendIcon from '@material-ui/icons/Send';
 import { IconButton } from '@material-ui/core';
+import axios from './axios';
+import Pusher from 'pusher-js'
+
 
 function App() {
   const [input,setInput]=useState("");
   const [messages,setMessages]=useState([]);
   const [username,setUsername]=useState("");
 
-  useEffect(()=>{
-    
+  const pusher = new Pusher('efc57b210ee48de83337', {
+    cluster: 'ap2'
+  });
+
+
+  useEffect(() => {
+    const channel = pusher.subscribe('messages');
+    channel.bind('newMessages', function(data) {
+      sync();
+    });
+  },[username])
+
+  const sync=async ()=>{
+    await axios.get("/retrieve/conversation")
+    .then((res)=>{
+      console.log(res.data);
+      setMessages(res.data);
+    })
+  }
+
+  useEffect(() => {
+    sync();
   },[])
+
 
   useEffect(() => {
     setUsername(prompt("Enter your name"));
   }, []);
 
   const sendMessage=(event)=>{
+    event.preventDefault();
+    axios.post("/save/messages/",{
+      username:username,
+      message:input,
+      timestamp:Date.now()
+    })
     setInput("");
   }
   return (
@@ -38,8 +68,12 @@ function App() {
       </form>
       <FlipMove>
       {
-        messages.map(({id,message})=>(
-          <Message key={id} username={username} message={message} />     
+        messages.map((message)=>(
+          <Message 
+            key={message._id} 
+            onClick={sendMessage}
+            username={username} 
+            message={message} />     
         ))
       }
       </FlipMove>
